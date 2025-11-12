9628f600fb7c264e8b08f83613d8593f972779ca
João Gabriel Feliciano <144286408+jgfeliciano@users.noreply.github.com>
2025-11-12 14:05:06 -0300

    feat(login): Add a login screen using Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MatProgressSpinnerModule
+    )
   ]
 };
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,16 +3,18 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, MatProgressSpinner],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   email = '';
   password = '';
+  isLoading = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -21,6 +23,8 @@ export class LoginComponent {
       alert('Preencher todos os campos');
       return;
     }
+
+    this.isLoading = true;
 
     this.authService.login(this.email, this.password).subscribe({
       next: (res) => {
